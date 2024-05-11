388769e99c308625252fdeee22f29bfbb83e2989
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-05-11 10:51:50 +0300

    Update Modul2hard.py

--- Modul2hard.py
+++ Modul2hard.py
@@ -1,4 +1,4 @@
-num = int(input("Какой число выпало? Введите для полученися пароля: \n"))
+num = int(input("Какоe число выпало? Введите для полученися пароля: \n"))
 
 list_ = [i for i in range(1,num)]
 new_list = []
@@ -11,7 +11,7 @@ def password_for_save_your_life_maker(num):
     for i in new_list:
         i.sort()
 
-    # new_list_2 = [i for i in new_list if i not in new_list_2]  Почему не получается?( Как это записать через list.comp ?
+    # new_list_2 = [i for i in new_list while i not in new_list_2]  Почему не получается?( Как это записать через list.comp ?
     new_list_2 = []
     for i in new_list:
         while i not in new_list_2 and i[0] != i[1]:
