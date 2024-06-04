508bb7b809e679100b00811c68f59c7a8ee814ad
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-06-04 08:59:57 +0300

    Add files via upload

---
+++ Homework_2.py
@@ -0,0 +1,11 @@
+
+class House():
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        numberOfFloors = floors
+        print(floors)
+h1 = House()
+h1.setNewNumberOfFloors(17)
+
