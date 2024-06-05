d45ed57cad76ae1168ebcc945529ff288fd99439
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-06-05 19:33:13 +0300

    Add files via upload

--- Homework_2.py
+++ Homework_2.py
@@ -4,8 +4,11 @@ class House():
         self.numberOfFloors = 0
 
     def setNewNumberOfFloors(self, floors):
-        numberOfFloors = floors
-        print(floors)
+        self.numberOfFloors += floors
+        print(self.numberOfFloors)
 h1 = House()
 h1.setNewNumberOfFloors(17)
+
+
+
 
