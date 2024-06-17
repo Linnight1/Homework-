389d7ba3ebb5c1caea6d5eb1a63f6774fa4c3b95
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-06-17 22:01:27 +0300

    Add files via upload

--- Homework_2.py
+++ Homework_2.py
@@ -1,14 +1,39 @@
+class Vehicle:
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+    def __init__(self,owner,__model,__color,__engine_power):
+        self.owner = owner
+        self.__model = __model
+        self.__engine_power = __engine_power
+        self.__color = __color
 
-class House():
-    def __init__(self):
-        self.numberOfFloors = 0
+    def get_model(self):
+        return (f"Модель {self.__model}")
 
-    def setNewNumberOfFloors(self, floors):
-        self.numberOfFloors += floors
-        print(self.numberOfFloors)
-h1 = House()
-h1.setNewNumberOfFloors(17)
+    def get_horsepower(self):
+        return (f"Мощность двигителя {self.__engine_power}")
+
+    def get_color(self):
+        return (f"Цвет {self.__color}")
+
+    def print_info(self):
+        print(f"{self.get_model()}\n{self.get_horsepower()}\n{self.get_color()}\nВладелец: {self.owner}")
 
 
 
+    def set_color(self, new_color):
+        self.new_color = new_color
+        if self.new_color in self._Vehicle__COLOR_VARIANTS or self.new_color.lower() in self._Vehicle__COLOR_VARIANTS :
+            self.__color = new_color
+        else:
+            print(f"Нельзя сменить цвет на {self.new_color}")
 
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
+vehicle1.print_info()
+vehicle1.set_color('Pink')
+vehicle1.set_color('BLACK')
+vehicle1.owner = 'Vasyok'
+vehicle1.print_info()
