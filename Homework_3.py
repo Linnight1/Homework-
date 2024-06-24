b8b7d3e0d9753c350f451a4feffdccf9059830cd
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-06-24 21:36:42 +0300

    Add files via upload

--- Homework_3.py
+++ Homework_3.py
@@ -5,25 +5,25 @@
 # Создайте экзмепляр класса Nissan и распечайте через функцию print vehicle_type, price
 
 class Vehicle:
-    def __init__(self,vehicle_type,price,horse_power):
+    def __init__(self,vehicle_type = None):
         self.vehicle_type = vehicle_type
-        self.vehicle_type = None
-        super().__init__(price,horse_power)
+
 class Car:
-    def __init__(self,price,horse_power):
+    def __init__(self, horse_power, price=1000000):
         self.price = price
-        self.price = 1000000
         self.horse_power = horse_power
 
     def horse_powers(self):
         return self.horse_power
+
 class Nissan(Vehicle,Car):
-    def __init__(self,vehicle_type,price,horse_power):
-        super().__init__(vehicle_type,price,horse_power)
-        self.vehicle_type = "car"
-        self.price = 900000
-        super().horse_powers()
-car1 = Nissan(1,2,3)
+    def __init__(self,price,vehicle_type,horse_power):
+        Vehicle.__init__(self,vehicle_type)
+        Car.__init__(self,price,horse_power)
+
+    def horse_powers(self):
+        print(self.horse_power)
+car1 = Nissan(900000,"car",120)
 
 print(car1.vehicle_type)
 print(car1.price)
