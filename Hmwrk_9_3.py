891141d0b07735bb69c9c9923e3d9785042705e6
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-07-10 22:35:41 +0300

    Add files via upload

---
+++ Hmwrk_9_3.py
@@ -0,0 +1,6 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+first_result = (len(i[0]) - len(i[1]) for i in zip(first,second) if len(i[0]) != len(i[1]))
+second_result = (len((first)[i]) == len((second)[i]) for i in range(0,3))
+print(list(first_result))
+print(list(second_result))
