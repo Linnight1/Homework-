d3fad9337f9821ebd21506ccefcb28de7d13d117
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-07-20 15:46:10 +0300

    Add files via upload

---
+++ Hmwrk_10_1.py
@@ -0,0 +1,34 @@
+from threading import Thread
+from datetime import datetime
+def write_words(word_count, file_name):
+    import time
+    with open(file_name, "a+", encoding="utf-8") as f:
+        for i in range(1, word_count + 1):
+            f.write("Какое-то слово № " + str(i) + "\n")
+            time.sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+time_start = datetime.now()
+write_words(10,"example1.txt")
+write_words(30, "example2.txt")
+write_words(200,"example3.txt")
+write_words(100, "example4.txt")
+time_end = datetime.now()
+print(f"Работа потоков {time_end - time_start}")
+thr_1 = Thread (target = write_words, args = (10, "example5.txt"))
+thr_2 = Thread (target = write_words, args = (30, "example6.txt"))
+thr_3 = Thread (target = write_words, args = (200, "example7.txt"))
+thr_4 = Thread (target = write_words, args = (100, "example7.txt"))
+time_start = datetime.now()
+
+thr_1.start()
+thr_2.start()
+thr_3.start()
+thr_4.start()
+
+thr_1.join()
+thr_2.join()
+thr_3.join()
+thr_4.join()
+time_end = datetime.now()
+print(f"Работа потоков {time_end - time_start}")
