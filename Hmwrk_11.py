c37629bce66f3eb0f700e0d1af1de02a715ce174
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-08-18 15:19:43 +0300

    Add files via upload

---
+++ Hmwrk_11.py
@@ -0,0 +1,26 @@
+import datetime
+import multiprocessing
+def read_info(name):
+    all_data = []
+    name = open(file,"r")
+    all_data.append(name.readlines())
+
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+start = datetime.datetime.now()
+for file in filenames:
+    read_info(file)
+end = datetime.datetime.now()
+time = end - start
+print(f" Линейный: {time}")
+
+if __name__ == "__main__":
+    with multiprocessing.Pool(processes = 4) as pool:
+        start2 = datetime.datetime.now()
+        pool.map(read_info, filenames)
+    end2 = datetime.datetime.now()
+    time2 = end2 - start2
+
+    print(f" Многопроцессорный: {time2}")
+
+
+
