97bf3e871db91154128d6d081e98aa0cdaf44b0d
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-08-28 12:38:53 +0300

    Add files via upload

---
+++ Hmwrk_12_1a.py
@@ -0,0 +1,27 @@
+#https://github.com/Linnight1/Homework-/blob/master/Hmwrk_12_1.py 
+
+import Hmwrk_12_1
+import unittest
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        self.some_obj = Hmwrk_12_1.Runner("obj")
+        for i in range(10):
+            Hmwrk_12_1.Runner.walk(self.some_obj)
+        self.assertEqual(self.some_obj.distance, 50)
+    def test_run(self):
+        self.some_obj = Hmwrk_12_1.Runner("obj")
+        for i in range(10):
+            Hmwrk_12_1.Runner.run(self.some_obj)
+        self.assertEqual(self.some_obj.distance, 100)
+
+    def test_challenge(self):
+        self.some_obj = Hmwrk_12_1.Runner("obj")
+        self.some_obj_2 = Hmwrk_12_1.Runner("obj2")
+        for i in range(10):
+            Hmwrk_12_1.Runner.run(self.some_obj)
+            Hmwrk_12_1.Runner.walk(self.some_obj_2)
+
+        self.assertNotEqual(self.some_obj.distance, self.some_obj_2.distance)
+
+if __name__ == "__main__":
+    unittest.main()
