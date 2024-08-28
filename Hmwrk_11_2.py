11ce678a2e4594b2b573e6d65717881f780a929e
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-08-28 11:38:06 +0300

    Add files via upload

--- Hmwrk_11_2.py
+++ Hmwrk_11_2.py
@@ -1,17 +1,24 @@
 import inspect
 from pprint import pprint
-
+class MyClass:
+    attr = "Class"
+    def __init__(self,obj):
+        self.obj = obj
+        self.instance_attr = "Instance"
+obj = MyClass(42)
 def introspection_info(obj):
     dict_info = {}
     dict_info["type"] = type(obj)
 
     dict_info["method"] = dir(obj)
     dict_info["module"] = inspect.getmodule(obj)
-    attr_list = []
-    for attr_name in dir(obj):
-        attr = getattr(obj, attr_name)
-        attr_list.append(attr)
-        dict_info["attr"] = attr_list
+    try:
+        dict_info["attr"] = obj.__dict__
+    except AttributeError:
+        dict_info["attr"] = None
+
     return dict_info
+
+pprint(introspection_info(obj))
 pprint(introspection_info(42))
 pprint(introspection_info([34,25,"oops"]))
