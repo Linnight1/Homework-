69754078bfa3bf153a50caf0a3df963eb42aca7e
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-08-28 14:17:40 +0300

    Add files via upload

---
+++ Hmwrk_12_2.py
@@ -0,0 +1,38 @@
+import test_12_2
+import unittest
+#https://github.com/Linnight1/Homework-/blob/master/test_12_2.py
+
+class TournamentTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.all_results = {}
+    def setUp(self):
+        self.runner_1 = test_12_2.Runner("Усейн", 10)
+        self.runner_2 = test_12_2.Runner("Андрей", 9)
+        self.runner_3 = test_12_2.Runner("Ник", 3)
+    @classmethod
+    def tearDownClass(self):
+        for key, value in self.all_results.items():
+            print(key,":",value)
+    def test_Tournament_1(self):
+        self.obj = test_12_2.Tournament(90,self.runner_1,self.runner_3)
+        self.all_results.update(self.obj.start())
+
+        self.assertTrue(self.all_results[max(self.all_results)],"Ник")
+
+    def test_Tournament_2(self):
+        self.obj = test_12_2.Tournament(90, self.runner_2, self.runner_3)
+        self.all_results.update(self.obj.start())
+
+        self.assertTrue(self.all_results[max(self.all_results)], "Ник")
+
+    def test_Tournament_3(self):
+        self.obj = test_12_2.Tournament(90, self.runner_1, self.runner_2, self.runner_3)
+        self.all_results.update(self.obj.start())
+
+        self.assertTrue(self.all_results[max(self.all_results)], "Ник")
+
+
+
+if __name__ == "__main__":
+    unittest.main()
