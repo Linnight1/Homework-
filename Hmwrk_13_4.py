38428934798c167311b93db241f33784c2fdeee6
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-09-02 12:38:52 +0300

    Add files via upload

---
+++ Hmwrk_13_4.py
@@ -0,0 +1,38 @@
+import asyncio
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(text="Calories")
+async def set_age(message):
+    await message.answer("Введите свой возраст")
+    await UserState.age.set()
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(first=message.text)
+    await message.answer("Введите свой рост")
+    await UserState.growth.set()
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(second=message.text)
+    await message.answer("Введите свой вес")
+    await UserState.weight.set()
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(third=message.text)
+    data = await state.get_data()
+    await message.answer(float(data['third']) * 10 + 6.25 * float(data['second']) - 5 * float(data['first']) - 161)
+    await state.finish()
+
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
