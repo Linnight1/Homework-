7134fa60f23fbc1854f85d5832d326398631de6f
Linnight1 <168333598+Linnight1@users.noreply.github.com>
2024-09-28 20:23:10 +0300

    Add files via upload

---
+++ Homework_14_1.py
@@ -0,0 +1,30 @@
+import sqlite3
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email On Users (email)")
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users (username, email, age, balance) VALUES (?,?,?,?)",
+                  (f"User{i}", f"example{i}@mail.com", f'{i * 10}', '1000'))
+# cursor.execute("DELETE FROM Users WHERE age > ?", ("1",))
+cursor.execute("UPDATE Users SET balance = ? WHERE  id % ? ", ("500", "2"))
+# cursor.execute("DELETE FROM Users WHERE not age % ?", ("3",))
+for i in range(1, 11, 3):
+    cursor.execute("DELETE FROM Users WHERE id = ?", (i,))
+
+cursor.execute("SELECT * FROM Users WHERE age != ?",  (60,))
+users = cursor.fetchall()
+for user in users:
+    print(user)
+
+connection.commit()
+connection.close()
